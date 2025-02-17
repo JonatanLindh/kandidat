55b7a078cf4dd21e04af941baef3099e76866d80
Kimshii <117647269+Kimshii@users.noreply.github.com>
2025-02-17 19:08:19 +0100

    add script for creating planet noise

---
+++ godot/src/noise/DisplayNoise.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System.Collections.Generic;
+
+[Tool]
+public partial class DisplayNoise : Node
+{
+    private bool _reload;
+    [Export]
+    public bool reload
+    {
+        get => _reload;
+        set
+        {
+            _reload = !value;
+            UpdateNoise();
+        }
+    }
+
+    List<Vector3> noise = new List<Vector3>();
+
+    public override void _Ready()
+    {
+        Display(new Vector3(0,0,0));
+    }
+
+    private void UpdateNoise()
+    {
+        GD.Print("Updated noise");
+        Node nod = GetNode<Node>("%CreateNoise");
+        PlanetNoise n = nod as PlanetNoise;
+        //GD.Print(n);
+        //PlanetNoise n = new PlanetNoise();
+        GD.Print(n);
+
+        foreach (Vector3 pos in n.GetNoise())
+        {
+            Display(pos);
+        }
+    }
+
+    // Add a debug sphere at global location.
+
+    private void Display(Vector3 position)
+    {
+        int size = 1;
+        //Node3D displayNode = GetNode<Node3D>("PlanetNoise/DisplayNoise");
+
+        SphereMesh sphere = new SphereMesh();
+        sphere.RadialSegments = 8;
+        sphere.Rings = 8;
+        sphere.Radius = size;
+        sphere.Height = size * 2;
+
+
+        MeshInstance3D node = new MeshInstance3D();
+
+        node.Mesh = sphere;
+        node.Translate(new Vector3(position.X, position.Y, position.Z));
+
+        node.Name = "noise";
+
+        this.AddChild(node);
+    }
+}
---
+++ godot/src/noise/PlanetNoise.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class PlanetNoise : Node
+{
+
+    public List<Vector3> GetNoise()
+    {
+        return new List<Vector3>();
+        //return CreatePlanetNoise();
+    }
+
+    private List<Vector3> CreatePlanetNoise()
+    {
+        List<Vector3> noise = new List<Vector3>();
+
+        FastNoiseLite n = new FastNoiseLite();
+        n.NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin;
+
+        int radius = 1;
+        float theta = 2 * Mathf.Pi;
+        float phi = Mathf.Pi;
+        float stepSize = 1 / 8;
+
+        for(float i = 0; i < theta; i += stepSize)
+        {
+            for(float j = 0; j < phi; j += stepSize)
+            {
+                for(float k = 0; k < radius; k += stepSize)
+                {
+                    noise.Add(new Vector3(radius * Mathf.Cos(i) * Mathf.Sin(j), radius * Mathf.Cos(j), radius * Mathf.Sin(theta) * Mathf.Sin(phi))); 
+
+                }
+            }
+        }
+
+        //TO GET NOISE: n.GetNoise3Dv()
+
+
+        return noise;
+
+
+    }
+
+}
