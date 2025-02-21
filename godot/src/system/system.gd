e9d8a781de16a12b93f86bbb48e5c38bba48e187
pau <paul.e.soukup@gmail.com>
2025-02-21 12:23:59 +0100

    Created "System" scene and ability to generate planets (bugfix needed) Generated planets arent affected by gravity when running the game !??!

---
+++ godot/src/system/system.gd
@@ -0,0 +1,65 @@
+@tool
+extends Node3D
+
+var planets = [];
+@export var generate: bool:
+	set(val):
+		print("dawg");
+		generatePlanet();
+		
+@export var clear: bool:
+	set(val):
+		for p in planets:
+			remove_child(p)
+		planets.clear();
+		
+
+const G = 1.0;
+const SUN_MASS = 1000.0;
+var PLANET_SCENE:PackedScene = load("res://src/bodies/planet/planet.tscn");
+var rand = RandomNumberGenerator.new();
+
+
+
+func orbitRadiusFromSpeed(v:float):
+	return SUN_MASS*G/pow(v,2)
+
+func orbitSpeedFromRadius(r:float):
+	return sqrt(SUN_MASS*G/r)
+
+func randomOrbitRadius():
+	return rand.randf_range(50,80);
+	
+func randomOrbitAngle():
+	return rand.randf_range(0, 2*PI);
+	
+func randomPlanetMass():
+	return rand.randf_range(1,2);
+
+func randomPlanetRadius():
+	return rand.randf_range(1,5);
+
+
+func generatePlanet(planetRadius = 0, planetMass = 0, orbitRadius = 0, orbitSpeed= 0):
+	#Planet stuff
+	if (planetRadius == 0):
+		planetRadius = randomPlanetRadius();
+	if (planetMass == 0):
+		planetMass = randomPlanetMass();
+	
+	#Orbit stuff
+	var orbitAngle = randomOrbitAngle();
+	if (orbitRadius == 0):
+		orbitRadius = randomOrbitRadius();
+	if (orbitSpeed == 0):
+		orbitSpeed = orbitSpeedFromRadius(orbitRadius);
+	var planetInstance = PLANET_SCENE.instantiate();
+	planetInstance.mass = planetMass;
+	planetInstance.velocity = Vector3(cos(orbitAngle)*orbitSpeed,0,sin(orbitAngle)*orbitSpeed)
+	planetInstance.position = Vector3(sin(orbitAngle)*orbitRadius,0,cos(orbitAngle)*orbitRadius)
+	planetInstance.planet_data.radius = planetRadius
+	add_child(planetInstance);
+	planetInstance.owner = self
+	planetInstance.add_to_group('gravity_body');
+	planets.append(planetInstance);
+	
