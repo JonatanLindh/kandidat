c3a1aa53ce1cef9658cca481ddc133bb458ea8c7
pau <paul.e.soukup@gmail.com>
2025-03-03 17:02:35 +0100

    Fixed orbit velocity. Orbits are now stable.

--- godot/src/system/system.gd
+++ godot/src/system/system.gd
@@ -2,7 +2,9 @@
 extends Node3D
 
 @export var planets = [];
-@export var numberOfPlanets:int = 0
+@export var numberOfPlanets:int = 5;
+@export var distanceBetweenPlanets = 25;
+@export var baseDistanceFromSun = 50;
 @export var generate: bool:
 	set(val):
 		print("dawg");
@@ -10,29 +12,35 @@ extends Node3D
 		
 @export var clear: bool:
 	set(val):
-		for p in planets:
-			var nodeName = "./GravityController/Planet" + str(p);
-			var pnode = get_node(nodeName)
-			if (is_instance_valid(pnode)):
-				pnode.queue_free()
-		planets.clear();
+		clearPlanets();
 		
+@onready var SUN = ($"./GravityController/Star")
 
 const G = 1.0;
-const SUN_MASS = 1000.0;
 var PLANET_SCENE:PackedScene = load("res://src/bodies/planet/planet.tscn");
 var rand = RandomNumberGenerator.new();
 
+func clearPlanets():
+	for p in planets:
+		var nodeName = "./GravityController/Planet" + str(p);
+		var pnode = get_node(nodeName)
+		if (is_instance_valid(pnode)):
+			pnode.queue_free()
+	planets.clear();
 
+#func _ready() -> void:
+
+	#clearPlanets();
+	#generatePlanets(5)
 
 func orbitRadiusFromSpeed(v:float):
-	return SUN_MASS*G/pow(v,2)
+	return SUN.mass*G/pow(v,2)
 
 func orbitSpeedFromRadius(r:float):
-	return sqrt(SUN_MASS*G/r)
+	return sqrt(SUN.mass*G/r)
 
 func randomOrbitRadius():
-	return rand.randf_range(50,80);
+	return rand.randf_range(2,80);
 	
 func randomOrbitAngle():
 	return rand.randf_range(0, 2*PI);
@@ -41,7 +49,7 @@ func randomPlanetMass():
 	return rand.randf_range(1,2);
 
 func randomPlanetRadius():
-	return rand.randf_range(1,5);
+	return rand.randf_range(1,2);
 
 
 func generatePlanet(planetRadius = 0, planetMass = 0, orbitRadius = 0, orbitSpeed= 0):
@@ -60,18 +68,16 @@ func generatePlanet(planetRadius = 0, planetMass = 0, orbitRadius = 0, orbitSpee
 	var randomID = rand.randi_range(100000, 999999);
 	var planetInstance = PLANET_SCENE.instantiate();
 	planetInstance.mass = planetMass;
-	planetInstance.velocity = Vector3(cos(orbitAngle)*orbitSpeed,0,sin(orbitAngle)*orbitSpeed)
+	planetInstance.velocity = Vector3(cos(orbitAngle)*orbitSpeed,0,-sin(orbitAngle)*orbitSpeed)
 	planetInstance.position = Vector3(sin(orbitAngle)*orbitRadius,0,cos(orbitAngle)*orbitRadius)
 	planetInstance.planet_data.radius = planetRadius
 	planetInstance.name = "Planet" + str(randomID);
-	planetInstance.trajectory_color = Color(rand.randi_range(0,255),rand.randi_range(0,255),rand.randi_range(0,255))
+	planetInstance.trajectory_color = Color.from_hsv(rand.randf_range(0,1),0.80,0.80);
 	$GravityController.add_child(planetInstance);
 	planetInstance.owner = self
 	planets.append(randomID);
 	
 
 func generatePlanets(n:int = 0):
-	var baseDistance = 30;
-	const step = 200;
 	for i in n:
-		generatePlanet(0,0,baseDistance + i*step);
+		generatePlanet(0,0,distanceBetweenPlanets + i*distanceBetweenPlanets);
