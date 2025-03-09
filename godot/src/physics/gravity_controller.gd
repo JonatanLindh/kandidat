ff707de3caf7e84495b82d4150f90b0d1fd33517
Jonatan Lindh <jonatan.lindh1@gmail.com>
2025-03-09 13:11:39 +0100

    trajectories ingame

--- godot/src/physics/gravity_controller.gd
+++ godot/src/physics/gravity_controller.gd
@@ -8,3 +8,20 @@ extends GravityController
 @export var ClearTrajectories: bool:
 	set(val):
 		self.clear_trajectories()
+
+var _t := 0.;
+var show_trajectories_ingame := false :
+	set(val):
+		show_trajectories_ingame = val
+		if !val:
+			self.clear_trajectories()
+		else:
+			self.simulate_trajectories()
+			_t = 0;
+
+func _process(delta: float) -> void:
+	_t += delta;
+	#FIXME: Should NOT block main thread
+	if _t >= 1  && show_trajectories_ingame:
+		_t = 0.;
+		self.simulate_trajectories()
