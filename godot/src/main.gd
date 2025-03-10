5e215cbbab14e835e927773365232c7df6fc9f28
pau <paul.e.soukup@gmail.com>
2025-03-10 21:23:59 +0100

    Changes Skybox is now black when in galaxy_map The distance limit when inside a system has been removed from galaxy_map, allowing infinite moving ss gets hidden when exploring a system which means it remains when someone exits, so they can see exactly which star they just visitied Player gets teleported above a system instead of inside the sun when exploring

--- godot/src/main.gd
+++ godot/src/main.gd
@@ -2,11 +2,19 @@ extends Node3D
 
 @onready var player = $Player;
 @onready var systemManager = $SystemManager;
-
+@onready var skybox = $Skybox;
 
 func _process(delta: float):
+	if systemManager.currentSeed != 0 and playerIsInOuterSpace():
+		systemManager.goToGalaxyMap();
+		skybox.environment= load("res://src/galaxy/skybox/_resources/space_no_stars.tres");
+		player.position = Vector3.ZERO;
+	
+
+func playerIsInOuterSpace():
 	var distance_from_origin = player.global_position.distance_to(Vector3.ZERO)
 	const distanceToOuterSpace = 200;
 	if distance_from_origin > systemManager.getSystemRadius() + distanceToOuterSpace:
-		systemManager.goToGalaxyMap();
-		player.position = Vector3.ZERO;
+		return true;
+	
+	return false;
