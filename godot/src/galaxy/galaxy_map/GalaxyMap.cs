b3efe01b78dc275dbcb4e1e071f90b1c027bae38
Jonatan Lindh <47460872+JonatanLindh@users.noreply.github.com>
2025-03-10 14:37:32 +0100

    Convert spaces to tabs in C# code
    Fixes #70
    
    Yes!
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/JonatanLindh/kandidat/issues/70?shareId=XXXX-XXXX-XXXX-XXXX).

--- godot/src/galaxy/galaxy_map/GalaxyMap.cs
+++ godot/src/galaxy/galaxy_map/GalaxyMap.cs
@@ -6,21 +6,21 @@ public partial class GalaxyMap : Node3D
 	InfiniteGalaxy galaxy;
 	UISelectableStar uiSelectableStar;
 
-    [Export] Node3D player;
+	[Export] Node3D player;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-        galaxy = GetNode<InfiniteGalaxy>("%InfiniteGalaxy");
-        galaxy.SetPlayer(player);
+		galaxy = GetNode<InfiniteGalaxy>("%InfiniteGalaxy");
+		galaxy.SetPlayer(player);
 
-        uiSelectableStar = GetNode<UISelectableStar>("%UiSelectableStar");
-        AddToGroup("GalaxyMap");
-    }
+		uiSelectableStar = GetNode<UISelectableStar>("%UiSelectableStar");
+		AddToGroup("GalaxyMap");
+	}
 
-    public void SelectedStarUI(SelectableStar star)
-    {
-        uiSelectableStar.SetStar(star);
-        uiSelectableStar.SetPlayer(player);
-    }
+	public void SelectedStarUI(SelectableStar star)
+	{
+		uiSelectableStar.SetStar(star);
+		uiSelectableStar.SetPlayer(player);
+	}
 }
