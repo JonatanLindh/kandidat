dc4fcf9c0074e66e8710fe7686307124f4c46325
William <56490562+w-william-w@users.noreply.github.com>
2025-03-21 01:18:10 +0100

    Implemented the Gpu & Cpu strategies for vertex generation

---
+++ godot/src/marchingcubes/CPU/CpuVerticesGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace Kandidat.marchingcubes.ComputeShader;
+
+public class CpuVerticesGenerator : IVerticesGenerationStrategy
+{
+    private float _isoLevel = 0f;
+    private int _scale = 1;
+    private float[,,] _datapoints;
+    private readonly List<Vector3> _vertices = new();
+
+
+    public List<Vector3> GenerateVertices(float[,,] datapoints, float isoLevel = 0f, int scale = 1)
+    {
+        _datapoints = datapoints;
+        _isoLevel = isoLevel;
+        _scale = scale;
+        _vertices.Clear();
+
+        for (var x = 0; x < _datapoints.GetLength(0) - 1; x++)
+        {
+            for (var y = 0; y < _datapoints.GetLength(1) - 1; y++)
+            {
+                for (var z = 0; z < _datapoints.GetLength(2) - 1; z++)
+                {
+                    MarchCube(x, y, z);
+                }
+            }
+        }
+        return _vertices;
+    }
+    private void MarchCube(int x, int y, int z)
+    {
+        var tri = GetTriangulation(x, y, z);
+        foreach (var edgeIndex in tri)
+        {
+            if (edgeIndex == -1) break;
+            var pointsIndices = MarchingTable.EDGES[edgeIndex];
+
+            var p0 = MarchingTable.POINTS[pointsIndices.X];
+            var p1 = MarchingTable.POINTS[pointsIndices.Y];
+			
+            var posA = new Vector3((x + p0.X) * _scale, (y + p0.Y) * _scale, (z + p0.Z) * _scale);
+            var posB = new Vector3((x + p1.X) * _scale, (y + p1.Y) * _scale, (z + p1.Z) * _scale);
+			
+            // TODO: use an actual interpolation function between the two points
+            var position = (posA + posB) / 2.0f;
+			
+            _vertices.Add(position);
+        }
+    }
+    
+    private int[] GetTriangulation(int x, int y, int z)
+    {
+        var idx = 0b00000000;
+        if (_datapoints[x, y, z] >= _isoLevel) idx |= 0b00000001;
+        if (_datapoints[x, y, z + 1] >= _isoLevel) idx |= 0b00000010;
+        if (_datapoints[x + 1, y, z + 1] >= _isoLevel) idx |= 0b00000100;
+        if (_datapoints[x + 1, y, z] >= _isoLevel) idx |= 0b00001000;
+		
+        if (_datapoints[x, y + 1, z] >= _isoLevel) idx |= 0b00010000;
+        if (_datapoints[x, y + 1, z + 1] >= _isoLevel) idx |= 0b00100000;
+        if (_datapoints[x + 1, y + 1, z + 1] >= _isoLevel) idx |= 0b01000000;
+        if (_datapoints[x + 1, y + 1, z] >= _isoLevel) idx |= 0b10000000;
+		
+        return MarchingTable.TRIANGULATIONS[idx];
+    }
+
+}
---
+++ godot/src/marchingcubes/ComputeShader/GpuVerticesGenerator.cs
@@ -0,0 +1,221 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot.Collections;
+using Array = Godot.Collections.Array;
+
+namespace Kandidat.marchingcubes.ComputeShader;
+public class GpuVerticesGenerator : IVerticesGenerationStrategy
+{
+	private float _isoLevel = 0f;
+	private int _size = 41;
+	private int _sizeX;
+	private int _sizeY;
+	private int _sizeZ;
+	private int _scale = 1;
+	private readonly List<Vector3> _vertices = new();
+	private float[,,] _dataPoints;
+	
+    // Shader Variables
+    private RenderingDevice _renderingDevice;
+    private Rid _shaderRid;
+    private Rid _pipeline;
+    private Rid _uniformSet;
+    private Rid _triangleBuffer;
+    private Rid _dataPointsBuffer;
+    private Rid _counterBuffer;
+    private Rid _paramBuffer;
+	
+    // Data received from Compute Shader
+    private float[] _triangles;
+    private int _triangleCount;
+    
+
+    ~GpuVerticesGenerator()
+	{
+		CleanupGpu();
+	}
+    public List<Vector3> GenerateVertices(float[,,] datapoints, float isoLevel = 0f, int scale = 1)
+    {
+	    _dataPoints = datapoints;
+	    _isoLevel = isoLevel;
+	    _scale = scale;
+	    _sizeX = datapoints.GetLength(0);
+	    _sizeY = datapoints.GetLength(1);
+	    _sizeZ = datapoints.GetLength(2);
+	    InitGpu();
+	    RunCompute();
+	    ProcessComputeData();
+	    CreateVertices();
+	    
+	    
+	    return _vertices;
+    }
+
+    private void InitGpu()
+    {
+        // Expensive operation, only do it once and cache the result
+
+        _renderingDevice = RenderingServer.CreateLocalRenderingDevice();
+
+        if (_renderingDevice == null)
+        {
+            OS.Alert("Couldn't create rendering device");
+            return;
+        }
+
+        _shaderRid = LoadShader(_renderingDevice, "res://src/marchingcubes/ComputeShader/MarchingCube.GLSL");
+        SetupBuffers();
+    }
+    private Rid LoadShader(RenderingDevice rd, string path)
+    {
+	    var shaderFileData = GD.Load<RDShaderFile>(path);
+	    var shaderSpirV = shaderFileData.GetSpirV();
+	    return rd.ShaderCreateFromSpirV(shaderSpirV);
+    }
+    private void SetupBuffers()
+	{
+		// Create Triangle Buffer
+		int maxTrisPerVoxel = 5;
+		int maxTriangles = maxTrisPerVoxel * (int)Math.Pow(_size, 3);
+		int floatsPerTriangle = sizeof(float) * 3;
+		int bytesPerTriangle = floatsPerTriangle * sizeof(float);
+		var maxBytes = new byte[bytesPerTriangle * maxTriangles];
+		
+		_triangleBuffer = _renderingDevice.StorageBufferCreate((uint)maxBytes.Length, maxBytes);
+		var triangleUniform = new RDUniform
+		{
+			UniformType = RenderingDevice.UniformType.StorageBuffer,
+			Binding = 0
+		};
+		triangleUniform.AddId(_triangleBuffer);
+		
+		// Create Data Points Buffer
+		var dataPoints = _dataPoints;
+		int totalDataPoints = dataPoints.GetLength(0) * dataPoints.GetLength(1) * dataPoints.GetLength(2);
+		var dataPointsBytes = new byte[totalDataPoints * sizeof(float)];
+
+		Buffer.BlockCopy(dataPoints, 0, dataPointsBytes, 0, totalDataPoints * sizeof(float));
+		
+		_dataPointsBuffer = _renderingDevice.StorageBufferCreate((uint)dataPointsBytes.Length, dataPointsBytes);
+		var dataPointsUniform = new RDUniform()
+		{
+			UniformType = RenderingDevice.UniformType.StorageBuffer,
+			Binding = 1
+		};
+		dataPointsUniform.AddId(_dataPointsBuffer);
+		
+		// Create Uniforms Buffer
+		var paramsArray = new float[]
+			{
+				_sizeX, 
+				_sizeY, 
+				_sizeZ, 
+				_isoLevel,
+				_scale,
+				0.0f, // Padding
+ 				0.0f, // Padding
+				0.0f // Padding
+			};
+		var paramBytes = new byte[paramsArray.Length * sizeof(float)];
+		Buffer.BlockCopy(paramsArray, 0, paramBytes, 0, paramBytes.Length);
+
+		_paramBuffer = _renderingDevice.UniformBufferCreate((uint)paramBytes.Length, paramBytes);
+		var paramUniform = new RDUniform()
+		{
+			UniformType = RenderingDevice.UniformType.UniformBuffer,
+			Binding = 2
+		};
+		paramUniform.AddId(_paramBuffer);
+		
+		// Create Counter Buffer
+		var counter = new uint[] {0};
+		var counterBytes = new byte[sizeof(uint)];
+		Buffer.BlockCopy(counter, 0, counterBytes, 0, counterBytes.Length);
+		_counterBuffer = _renderingDevice.StorageBufferCreate((uint)counterBytes.Length, counterBytes);
+		var counterUniform = new RDUniform()
+		{
+			UniformType = RenderingDevice.UniformType.StorageBuffer,
+			Binding = 3
+		};
+		counterUniform.AddId(_counterBuffer);
+		
+		var buffers = new Array<RDUniform>{triangleUniform, dataPointsUniform, paramUniform, counterUniform};
+		_uniformSet = _renderingDevice.UniformSetCreate(buffers, _shaderRid, 0);
+		_pipeline = _renderingDevice.ComputePipelineCreate(_shaderRid);
+	}
+    
+	private void RunCompute()
+	{
+		var computeList = _renderingDevice.ComputeListBegin();
+		_renderingDevice.ComputeListBindComputePipeline(computeList, _pipeline);
+		_renderingDevice.ComputeListBindUniformSet(computeList, _uniformSet, 0);
+
+		var groupsNeeded = (uint)Math.Ceiling(_size / 8.0f);
+		_renderingDevice.ComputeListDispatch(computeList, 
+			xGroups: groupsNeeded, 
+			yGroups: groupsNeeded, 
+			zGroups: groupsNeeded);
+		_renderingDevice.ComputeListEnd();
+
+		_renderingDevice.Submit();
+	}
+    
+	private void ProcessComputeData()
+	{
+		_renderingDevice.Sync();
+		
+		// Get Output Data
+		var counterData = _renderingDevice.BufferGetData(_counterBuffer);
+		_triangleCount = BitConverter.ToInt32(counterData, 0);
+		
+		var triangleData = _renderingDevice.BufferGetData(_triangleBuffer);
+		_triangles = new float[triangleData.Length / sizeof(float)];
+		Buffer.BlockCopy(triangleData, 0, _triangles, 0, triangleData.Length);
+	}
+	
+	private void CreateVertices()
+	{
+		var numVertices = _triangleCount * 3;
+		_vertices.Capacity = numVertices;
+		
+		for (int i = 0; i < _triangleCount; i++)
+		{
+			var triIndex = i * 12;
+			_vertices.Add(new Vector3(_triangles[triIndex], _triangles[triIndex + 1], _triangles[triIndex + 2]));
+			_vertices.Add(new Vector3(_triangles[triIndex + 4], _triangles[triIndex + 5], _triangles[triIndex+ 6]));
+			_vertices.Add(new Vector3(_triangles[triIndex + 8], _triangles[triIndex + 9], _triangles[triIndex + 10]));
+		}
+	}
+	
+	private void CleanupGpu()
+	{
+		if (_renderingDevice == null) return;
+		
+		_renderingDevice.FreeRid(_pipeline);
+		_pipeline = new Rid();
+		
+		_renderingDevice.FreeRid(_uniformSet);
+		_uniformSet = new Rid();
+
+		_renderingDevice.FreeRid(_shaderRid);
+		_shaderRid = new Rid();
+
+		_renderingDevice.FreeRid(_triangleBuffer);
+		_triangleBuffer = new Rid();
+
+		_renderingDevice.FreeRid(_dataPointsBuffer);
+		_dataPointsBuffer = new Rid();
+
+		_renderingDevice.FreeRid(_counterBuffer);
+		_counterBuffer = new Rid();
+
+		_renderingDevice.FreeRid(_paramBuffer);
+		_paramBuffer = new Rid();
+		
+		_renderingDevice.Free();
+		_renderingDevice = null;
+	}
+	
+}
