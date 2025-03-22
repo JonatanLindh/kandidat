a52e0cce4b9542a8bc5c4954420db2bad4ecbf62
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-03-22 12:04:43 +0100

    add StarFinder to GalaxyMap

--- godot/src/galaxy/galaxy_map/GalaxyMap.cs
+++ godot/src/galaxy/galaxy_map/GalaxyMap.cs
@@ -5,6 +5,10 @@ public partial class GalaxyMap : Node3D
 {
 	InfiniteGalaxy galaxy;
 	UISelectableStar uiSelectableStar;
+	
+	StarFinder starFinder;
+	float range = 1000;
+	float radius = 100;
 
 	Node3D player;
 
@@ -15,13 +19,30 @@ public partial class GalaxyMap : Node3D
 		this.player = GetTree().CurrentScene.GetNode<Node3D>("Player");
 		galaxy.SetPlayer(player);
 
+		starFinder = GetNode<StarFinder>("%StarFinder");
+		starFinder.galaxy = galaxy;
+
 		uiSelectableStar = GetNode<UISelectableStar>("%UiSelectableStar");
 		AddToGroup("GalaxyMap");
 	}
 
-	public void SelectedStarUI(SelectableStar star)
+	public override void _Input(InputEvent @event)
 	{
-		uiSelectableStar.SetStar(star);
-		uiSelectableStar.SetPlayer(player);
+		if(@event is InputEventMouseButton eventButton)
+		{
+			if(eventButton.Pressed && eventButton.ButtonIndex == MouseButton.Left)
+			{
+				Vector3 worldDir = GetViewport().GetCamera3D().ProjectPosition(eventButton.Position, 1).Normalized();
+				Star star = starFinder.FindStar(this.player.Position, worldDir * range, radius, radius);
+
+				if(star != null)
+				{
+					GD.Print($"Selected star: [Name: {star.name} | Position: {star.transform.Origin} | Seed: {star.seed}]");
+
+					uiSelectableStar.SetStar(star);
+					uiSelectableStar.SetPlayer(player);
+				}
+			}
+		}
 	}
 }
