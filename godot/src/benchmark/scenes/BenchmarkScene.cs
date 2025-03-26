4275cfb30bd6bba71f5cf047959267f7d4ada2f0
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-03-26 19:05:17 +0100

    move benchmark galaxy logic to new abstract class

--- godot/src/benchmark/scenes/BenchmarkScene.cs
+++ godot/src/benchmark/scenes/BenchmarkScene.cs
@@ -1,12 +1,36 @@
 using Godot;
 using System;
 
-public partial class BenchmarkScene : Node3D
+public abstract partial class BenchmarkScene : Node3D
 {
 	protected Benchmark benchmark;
+
+	[Export] float benchmarkDuration = 30.0f;
+	float currentTime = 0.0f;
+
+	public override void _Ready()
+	{
+		Ready();
+	}
+
+	public override void _Process(double delta)
+	{
+		if(currentTime > benchmarkDuration)
+		{
+			benchmark.ExitScene();
+			return;
+		}
+
+		Process(delta);
+
+		currentTime += (float)delta;
+	}
 
 	public void setBenchmark(Benchmark benchmark)
 	{
 		this.benchmark = benchmark;
 	}
+
+	public abstract void Ready();
+	public abstract void Process(double delta);
 }
--- godot/src/benchmark/scenes/GalaxyMapBenchmark.cs
+++ godot/src/benchmark/scenes/GalaxyMapBenchmark.cs
@@ -6,27 +6,21 @@ public partial class GalaxyMapBenchmark : BenchmarkScene
 	[Export] GalaxyMap galaxyMap;
 	[Export] Camera3D player;
 
-	[ExportGroup("CHANGES INVALIDATE OLD BENCHMARKS")]
+	[ExportCategory("CHANGES INVALIDATE OLD BENCHMARKS")]
 	[Export] float speed = 10000.0f;
-	[Export] float duration = 60.0f;
 	[Export] uint seed = 1234;
 
 	float currentTime = 0.0f;
 
-	public override void _Ready()
+	public override void Ready()
 	{
 		InfiniteGalaxy galaxy = galaxyMap.GetNode<InfiniteGalaxy>("InfiniteGalaxy");
 		galaxy.Set("player", player);
 		galaxy.Set("seed", seed);
 	}
 
-	public override void _Process(double delta)
+	public override void Process(double delta)
 	{
-		if(currentTime >= duration)
-		{
-			benchmark.ExitScene();
-		}
-
 		player.Position += Vector3.Forward * speed * (float)delta;
 		currentTime += (float)delta;
 	}
