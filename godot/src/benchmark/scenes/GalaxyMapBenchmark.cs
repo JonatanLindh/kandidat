095fe2b39f0e39692f0b68d442c675e3449338e0
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-03-26 17:15:04 +0100

    add galaxy benchmark warning

--- godot/src/benchmark/scenes/GalaxyMapBenchmark.cs
+++ godot/src/benchmark/scenes/GalaxyMapBenchmark.cs
@@ -6,14 +6,16 @@ public partial class GalaxyMapBenchmark : BenchmarkScene
 	[Export] GalaxyMap galaxyMap;
 	[Export] Camera3D player;
 
+	[ExportGroup("CHANGES INVALIDATE OLD BENCHMARKS")]
 	[Export] float speed = 10000.0f;
+	[Export] float duration = 60.0f;
 	[Export] uint seed = 1234;
+
 	float currentTime = 0.0f;
 
 	public override void _Ready()
 	{
 		InfiniteGalaxy galaxy = galaxyMap.GetNode<InfiniteGalaxy>("InfiniteGalaxy");
-
 		galaxy.Set("player", player);
 		galaxy.Set("seed", seed);
 	}
