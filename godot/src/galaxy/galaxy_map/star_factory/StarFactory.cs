e361beec75cb56c966bd748564bbe00824d2eeec
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-05 09:59:46 +0200

    add StarFactory.cs docs

--- godot/src/galaxy/galaxy_map/star_factory/StarFactory.cs
+++ godot/src/galaxy/galaxy_map/star_factory/StarFactory.cs
@@ -3,6 +3,12 @@ using System;
 
 public class StarFactory
 {
+	/// <summary>
+	/// Creates a star at the given position with a new seed based on the galaxy seed and position.
+	/// </summary>
+	/// <param name="position"></param>
+	/// <param name="galaxySeed"></param>
+	/// <returns></returns>
 	public Star CreateStar(Vector3 position, uint galaxySeed)
 	{
 		Transform3D starTransform = new Transform3D(Basis.Identity, position);
@@ -14,12 +20,22 @@ public class StarFactory
 		return star;
 	}
 
+	/// <summary>
+	/// Generates a unique seed for the star based on the galaxy seed and its position.
+	/// </summary>
+	/// <param name="galaxySeed"></param>
+	/// <param name="position"></param>
+	/// <returns></returns>
 	private uint GenerateSeed(uint galaxySeed, Vector3 position)
 	{
 		SeedGenerator seedGen = new SeedGenerator();
 		return seedGen.GenerateSeed(galaxySeed, position);
 	}
 
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
 	private string GenerateName()
 	{
 		return "Star";
