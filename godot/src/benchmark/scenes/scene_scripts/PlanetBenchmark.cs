a4bf2c1c7195ff42fa85b0a8c996ae7812215e82
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-09 17:02:57 +0200

    add planet benchmark orbit

--- godot/src/benchmark/scenes/scene_scripts/PlanetBenchmark.cs
+++ godot/src/benchmark/scenes/scene_scripts/PlanetBenchmark.cs
@@ -3,19 +3,21 @@ using System;
 
 public partial class PlanetBenchmark : BenchmarkScene
 {
-	[Export] PlanetMarchingCube planet;
-	[Export] Camera3D player;
+	[Export] Node3D orbitPivot;
+	[Export] Node3D planetPivot;
 
-	[Export] float speed = 1500.0f;
+	[Export] float orbitSpeed = 5.0f;
+	[Export] float planetSpeed = 15.0f;
 	[Export] uint seed = 420;
 
 	public override void BenchmarkReady()
 	{
-		//planet.
+
 	}
 
 	public override void BenchmarkProcess(double delta)
 	{
-		//player.Position += Vector3.Forward * speed * (float)delta;
+		orbitPivot.RotateY(Mathf.DegToRad(orbitSpeed * (float)delta));
+		planetPivot.RotateZ(Mathf.DegToRad(-planetSpeed * (float)delta));
 	}
 }
