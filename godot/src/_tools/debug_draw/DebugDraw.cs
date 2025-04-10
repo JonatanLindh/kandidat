634b972d31ff9950d7092c01a2666599300bb52b
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-10 12:27:59 +0200

    add draw debug line

--- godot/src/_tools/debug_draw/DebugDraw.cs
+++ godot/src/_tools/debug_draw/DebugDraw.cs
@@ -4,6 +4,7 @@ using System;
 public partial class DebugDraw : Node
 {
 	[Export] StandardMaterial3D sphereMaterial;
+	[Export] StandardMaterial3D lineMaterial;
 
 	public void Clear()
 	{
@@ -15,31 +16,32 @@ public partial class DebugDraw : Node
 
 	public void DrawSphere(Vector3 position, float radius)
 	{
-		MeshInstance3D mesh = CreateSphere(position, radius, sphereMaterial);
-		AddChild(mesh);
-	}
-
-	public void DrawSphere(Vector3 position, float radius, Color color)
-	{
-		sphereMaterial.AlbedoColor = color;
-		MeshInstance3D mesh = CreateSphere(position, radius, sphereMaterial);
-		AddChild(mesh);
-	}
-
-	private MeshInstance3D CreateSphere(Vector3 position, float radius, StandardMaterial3D material)
-	{
-		MeshInstance3D meshInstance = new MeshInstance3D();
+		MeshInstance3D mesh = new MeshInstance3D();
 		SphereMesh sphereMesh = new SphereMesh();
 
 		sphereMesh.Radius = radius;
 		sphereMesh.Height = radius * 2;
 
-		meshInstance.Mesh = sphereMesh;
-		meshInstance.MaterialOverride = sphereMaterial;
-		meshInstance.Position = position;
+		mesh.Mesh = sphereMesh;
+		mesh.MaterialOverride = sphereMaterial;
+		mesh.Position = position;
 
-		return meshInstance;
+		AddChild(mesh);
 	}
 
+	public void DrawLine(Vector3 from, Vector3 to)
+	{
+		MeshInstance3D mesh = new MeshInstance3D();
+		ImmediateMesh immediateMesh = new ImmediateMesh();
 
+		immediateMesh.SurfaceBegin(Mesh.PrimitiveType.Lines);
+		immediateMesh.SurfaceAddVertex(from);
+		immediateMesh.SurfaceAddVertex(to);
+		immediateMesh.SurfaceEnd();
+
+		mesh.Mesh = immediateMesh;
+		mesh.MaterialOverride = lineMaterial;
+
+		AddChild(mesh);
+	}
 }
