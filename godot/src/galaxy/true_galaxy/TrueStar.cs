295dd72fd9cba295983a0f4f2ee89473210cb08b
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-11 10:05:32 +0200

    made star mass public

--- godot/src/galaxy/true_galaxy/TrueStar.cs
+++ godot/src/galaxy/true_galaxy/TrueStar.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class TrueStar : Star
 {
-	public float mass { get; private set; }
+	public float mass { get; set; }
 	public Vector3 direction { get; set; }
 
 	public TrueStar(Transform3D transform, uint seed, float mass, Vector3 direction, string name = "Star")
