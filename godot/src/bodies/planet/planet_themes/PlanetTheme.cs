304d7bc58263aa03c382df9586b0f391d5881825
erbergli <erik.berglind@gmail.com>
2025-04-11 13:16:08 +0200

    Start with theme resources

---
+++ godot/src/bodies/planet/planet_themes/PlanetTheme.cs
@@ -0,0 +1,13 @@
+// PlanetTheme.cs
+using Godot;
+using System.Collections.Generic;
+
+[GlobalClass]
+public partial class PlanetTheme : Resource
+{
+    [Export]
+    public Godot.Collections.Array<Color> Colors { get; set; } = new Godot.Collections.Array<Color>();
+
+    [Export]
+    public Vector3 AtmosphereWavelength { get; set; } = Vector3.Zero;
+}
