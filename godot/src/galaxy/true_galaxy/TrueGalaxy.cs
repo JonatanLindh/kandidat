d1af601769d3c0845e6e3de997ae94e2bbeaa405
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-11 09:50:00 +0200

    add true galaxy star get/set

--- godot/src/galaxy/true_galaxy/TrueGalaxy.cs
+++ godot/src/galaxy/true_galaxy/TrueGalaxy.cs
@@ -3,5 +3,63 @@ using System;
 
 public partial class TrueGalaxy : Node3D
 {
+	DiscGalaxy discGalaxy;
+	TrueStar[] stars;
+	bool starsInitialized = false;
 
+	public override void _Ready()
+	{
+		discGalaxy = GetNode<DiscGalaxy>("%DiscGalaxy");
+		CallDeferred(nameof(InitializeStars));
+	}
+
+	private void InitializeStars()
+	{
+		stars = discGalaxy.GetStars();
+		starsInitialized = true;
+	}
+
+	public override void _Process(double delta)
+	{
+
+	}
+
+	/// <summary>
+	/// To get the stars of the galaxy.
+	/// </summary>
+	/// <returns></returns>
+	public TrueStar[] GetStars()
+	{
+		if (!starsInitialized)
+		{
+			GD.PrintErr("Stars not initialized yet.");
+			return null;
+		}
+
+		return stars;
+	}
+
+	/// <summary>
+	/// To update the stars to a new array of stars.
+	/// Redraws the multimesh as well.
+	/// </summary>
+	/// <param name="newStars"></param>
+	public void SetStars(TrueStar[] newStars)
+	{
+		if (!starsInitialized)
+		{
+			GD.PrintErr("Stars not initialized yet.");
+			return;
+		}
+
+		if (newStars.Length != stars.Length)
+		{
+			GD.PrintErr("New stars array length does not match the existing stars array length.");
+			return;
+		}
+
+		stars = newStars;
+
+		RedrawStars();
+	}
 }
