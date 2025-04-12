560edbc9ec00578c5028bcfab036530eb3435b64
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-12 11:53:47 +0200

    add star distribution in arms

--- godot/src/galaxy/galaxies/modular_galaxy/ModularGalaxy.cs
+++ godot/src/galaxy/galaxies/modular_galaxy/ModularGalaxy.cs
@@ -4,8 +4,10 @@ using System;
 public partial class ModularGalaxy : Node3D
 {
 	[Export] StarMultiMesh starMultiMesh;
+	[Export] Mesh starMesh;
 	[Export] ModularGalaxyDistribution distribution;
 	[Export] uint seed;
+	Vector3[] _stars;
 
 	public override void _Ready()
 	{
@@ -13,11 +15,79 @@ public partial class ModularGalaxy : Node3D
 		if (seed == 0) seed = (uint)new Random().Next();
 		GD.Seed(seed);
 
-		Generate();
+		_stars = Generate();
+		DrawStars(_stars);
 	}
 
-	private void Generate()
+	private void DrawStars(Vector3[] stars)
 	{
+		if (starMultiMesh == null)
+		{
+			GD.PrintErr("ModularGalaxy: StarMultiMesh is not initialized.");
+			return;
+		}
 
+		if (stars.Length == 0)
+		{
+			GD.PrintErr("ModularGalaxy: No stars to draw.");
+			return;
+		}
+
+		starMultiMesh.DrawStars(stars, starMesh);
+	}
+
+	private Vector3[] Generate()
+	{
+		Vector3[] stars = new Vector3[distribution.starCount];
+
+		for (int i = 0; i < distribution.starCount; i++)
+		{
+			Vector3 point = SamplePoint();
+			stars[i] = point;
+		}
+
+		return stars;
+	}
+
+	private Vector3 SamplePoint()
+	{
+		Vector3 point = Vector3.Zero;
+		double armAngle =  2 * Math.PI / distribution.armCount;
+
+		bool valid = false;
+		while(!valid)
+		{
+			// Sample a random point from within the galaxy size
+			float x = (float)GD.RandRange(-distribution.galaxySize, distribution.galaxySize);
+			float y = (float)GD.RandRange(-distribution.galaxySize, distribution.galaxySize);
+			float z = (float)GD.RandRange(-distribution.galaxySize, distribution.galaxySize);
+			point = new Vector3(x, y, z);
+
+			double angle = Math.Atan2(point.Z, point.X);
+
+			for (double i = -Math.PI; i < Math.PI; i += armAngle)
+			{
+				if(angle > i && angle < i + armAngle * distribution.armWidth)
+				{
+					valid = true;
+					break;
+				}
+
+			}
+		}
+
+		point.Y = 0;
+
+
+
+		
+
+
+		return point;
+	}
+
+	public Vector3[] GetStars()
+	{
+		return _stars;
 	}
 }
--- godot/src/galaxy/galaxies/modular_galaxy/star_distribution/ModularGalaxyDistribution.cs
+++ godot/src/galaxy/galaxies/modular_galaxy/star_distribution/ModularGalaxyDistribution.cs
@@ -5,4 +5,8 @@ using System;
 public partial class ModularGalaxyDistribution : Resource
 {
 	[Export] public int starCount { get; private set; }
+	[Export] public int galaxySize { get; private set; }
+
+	[Export] public int armCount { get; private set; }
+	[Export] public float armWidth { get; private set; }
 }
