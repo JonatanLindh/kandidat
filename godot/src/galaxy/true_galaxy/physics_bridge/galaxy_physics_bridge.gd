84a836410a7a7f5b8709a2234925c4a6e8c6dcc7
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-04-15 11:04:09 +0200

    change physics bridge interaction to take in velocities

--- godot/src/galaxy/true_galaxy/physics_bridge/galaxy_physics_bridge.gd
+++ godot/src/galaxy/true_galaxy/physics_bridge/galaxy_physics_bridge.gd
@@ -16,9 +16,9 @@ func init_stars():
 ##
 ## [codeblock]
 ## Godot.Collections.Dictionary dict {
-##     {"transform", Godot.Collections.Array<Transform3D>},
-##     {"velocity", Godot.Collections.Array<Vector3>},
-##     {"mass", Godot.Collections.Array<float>}
+##     {"position", PackedVector3Array},
+##     {"velocity", PackedVector3Array},
+##     {"mass", PackedFloat32Array}
 ## }
 ## [/codeblock]
 ##
@@ -34,18 +34,9 @@ func get_stars() -> Dictionary:
 func get_delta() -> float:
 	return get_process_delta_time()
 
-## Updates star transforms and velocity
-##
-## [codeblock]
-## Godot.Collections.Dictionary dict {
-##     {"transform", Transform3D},
-##     {"velocity", Vector3},
-##     {"mass", float} # Nothing is done with mass.
-## }
-## [/codeblock]
-##
-func update_stars(new_stars : Dictionary):
+## Updates star velocities and applies them to the transforms
+func apply_velocities(new_velocites : PackedVector3Array):
 	if(!init):
 		printerr("Physics bridge: Stars not initialized yet.")
 		return
-	true_galaxy.UpdateStars(new_stars)
+	true_galaxy.ApplyVelocities(new_velocites)
