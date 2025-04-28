ff57196291f45cadeea19a6bcc73e91a4dc34fad
Kimshii <117647269+Kimshii@users.noreply.github.com>
2025-04-28 19:59:02 +0200

    add crude underwater shader, kind of works sometimes

--- godot/src/ocean/underwater_filter.gd
+++ godot/src/ocean/underwater_filter.gd
@@ -2,24 +2,16 @@
 extends MeshInstance3D
 class_name underwater_filter
 
-@onready var _camera: Camera3D = $Head/Camera3D
+@onready var _camera: Camera3D;
 var _filter_quad: QuadMesh = mesh as QuadMesh;
 
 func _ready() -> void:
-	#_camera = get_node("root/Main/Player/Head/Camera3D");
-	setup_filter();
+	_setup_filter();
 
-func setup_filter() -> void:
-	#var camera_fov: float = _camera.fov;
-	#var near_plane_distance: float = _camera.near;
-	#var far_plane_distance: float = _camera.far;
-	#var aspect_ratio: float = _camera.get_viewport().Size.x 
+func _setup_filter() -> void:
+	_camera = get_parent();
 	var screen_size: Vector2 = get_viewport().get_visible_rect().size;
 	_filter_quad.size = screen_size;
 
-func _process(delta: float) -> void:
-	if(_inside_ocean()):
-		1+1
-		
-func _inside_ocean():
-	1+1
+func toggle_underwater_filter(toggle: bool):
+	visible = toggle;
