9f0146fddd26a3e1de22dcf9076a1cc06a67c85c
erbergli <112847802+erbergli@users.noreply.github.com>
2025-05-01 13:00:48 +0200

    Fix invisible atmospheres

--- godot/src/bodies/planet/atmosphere/atmosphere.gd
+++ godot/src/bodies/planet/atmosphere/atmosphere.gd
@@ -39,7 +39,7 @@ func _ready() -> void:
 	#_update_atmopshere_color(random_index)
 	
 	mesh.material.set_shader_parameter("planet_radius", radius)
-	mesh.size = Vector3(radius*0.2,radius*0.2,radius*0.2)
+	mesh.size = Vector3(radius*2,radius*2,radius*2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
