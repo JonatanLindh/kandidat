2e0d9b87442b023d587660ca873f5bf5d7d8f1de
Kimshii <117647269+Kimshii@users.noreply.github.com>
2025-05-06 14:32:27 +0200

    pad boarders before generating points

--- godot/src/noise/CelestialBodyNoise/PlanetNoise/PlanetNoise.cs
+++ godot/src/noise/CelestialBodyNoise/PlanetNoise/PlanetNoise.cs
@@ -1,5 +1,8 @@
 using Godot;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 /// <summary>
 /// A class for creating different types of noise to be used when generating planets
@@ -23,34 +26,31 @@ public partial class PlanetNoise
         float[,,] points = new float[width, height, depth];
         Vector3 centerPoint = new Vector3I(radius, radius, radius);
 
-         // creates a cube of points
-         for (int x = 0; x < width; x++)
-         {
-             for (int y = 0; y < height; y++)
-             {
-                 for (int z = 0; z < depth; z++)
-                 {
-                    // Pad the boarders of the cube with empty space so marching cubes correctly generates the mesh at the edges
-                    if (x == 0 || x == width - 1 || y == 0 || y == height - 1 || z == 0 || z == depth - 1)
-                    {
-                        points[x, y, z] = -1.0f;
-                        continue;
-                    }
+        // Pad the boarders of the cube with empty space so marching cubes correctly generates the mesh at the edges
+        PadBoardersWithAir(points, width, height, depth);
 
+        Stopwatch watch = new Stopwatch();
+        // boarders are already padded, so only need to iterate from [1, size-1)
+        for (int x = 1; x < width - 1; x++)
+        {
+            for (int y = 1; y < height - 1; y++)
+            {
+                for (int z = 1; z < depth - 1; z++)
+                {
                     // Calculate distance from center of planet to the point (x,y,z)
-                    Vector3 currentPosition = new Vector3I(x, y, z);
-                    float distanceToCenter = (centerPoint - currentPosition).Length();
+                    Vector3 currentPoint = new Vector3I(x, y, z);
+                    float distanceToCenter = (centerPoint - currentPoint).Length();
                     float distanceAwayFromCenter = (float)radius - distanceToCenter;
 
                     // Apply fbm to layer noise
-                    float value = Fbm(distanceAwayFromCenter, currentPosition, param, fastNoise);
+                    float value = Fbm(distanceAwayFromCenter, currentPoint, param, fastNoise);
 
                     // Update point (x,y,z) with value from fbm
                     points[x, y, z] = value;
                 }
             }
-         }
-        
+        }
+
         return points;
     }
 
@@ -86,6 +86,44 @@ public partial class PlanetNoise
         }
 
         return valueAfterFbm;
+    }
+
+    /// <summary>
+    /// Pads the array with "air" (-1.0) at the edges of the array, i.e. x,y,z = 0 & x,y,z = size-1
+    /// </summary>
+    /// <param name="arrayToPadWithAir"></param>
+    /// <param name="size"></param>
+    private static void PadBoardersWithAir(float[,,] arrayToPadWithAir, int width, int height, int depth)
+    {
+        int widthEdge = width - 1;
+        for (int y = 0; y < height; y++)
+        {
+            for(int z = 0; z < depth; z++)
+            {
+                arrayToPadWithAir[0, y, z] = -1.0f;
+                arrayToPadWithAir[widthEdge, y, z] = -1.0f;
+            }
+        }
+
+        int heightEdge = height - 1;
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                arrayToPadWithAir[x, 0, z] = -1.0f;
+                arrayToPadWithAir[x, heightEdge, z] = -1.0f;
+            }
+        }
+
+        int depthEdge = depth - 1;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                arrayToPadWithAir[x, y, 0] = -1.0f;
+                arrayToPadWithAir[x, y, depthEdge] = -1.0f;
+            }
+        }
     }
 
     public ImageTexture3D Get3DNoiseTexture(FastNoiseLite fastNoise, int width, int height, int depth, bool useMipmaps)
