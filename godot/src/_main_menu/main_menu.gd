01cf0ac66134d29d70599c3f5ab4351e2ab08eea
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2025-05-23 14:57:47 +0200

    add basic main menu

---
+++ godot/src/_main_menu/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+@onready var system : Node3D = $System
+
+@onready var seed_text_edit : TextEdit = $CanvasLayer/PanelContainer/VBoxContainer/SeedTextEdit
+@onready var start_button : Button = $CanvasLayer/PanelContainer/VBoxContainer/StartButton
+
+@export var start_scene : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	HudSignalBus.emit_signal("orbits_visibility", true)
+	system.generateSystemFromSeed(420)
+	
+
+func _on_start_button_pressed() -> void:
+	GameSettings.SEED = seed_text_edit.text
+	get_tree().change_scene_to_packed(start_scene)
